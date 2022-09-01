d939b92644f773b0c2a7c1f89e965cd31aff8148
Tom Roar Furunes <tom.roar@halloverden.no>
2022-09-01 18:58:55 +0200

    EDIT: move services yaml + cleanup

--- src/DependencyInjection/HalloVerdenRouteDeprecationExtension.php
+++ src/DependencyInjection/HalloVerdenRouteDeprecationExtension.php
@@ -16,7 +16,7 @@ class HalloVerdenRouteDeprecationExtension extends Extension {
    * @throws \Exception
    */
   public function load(array $configs, ContainerBuilder $container) {
-    $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
     $loader->load('services.yaml');
   }
 
